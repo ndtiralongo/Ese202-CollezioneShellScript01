046d6050b111ebde0bd89cd40ab37ce50f57f3f5
ndtiralongo <Andreatiralongo12@gmail.com>
2015-03-09 14:53:28 +0100

    Update script01_01.sh

--- script01_01.sh
+++ script01_01.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 # Riceve un argomento da riga di comando.
+
+echo "Inserire un argomento"
+exit 1
+
 # Se la directory corrente contiene il file il cui nome e' passato come argomento, stampa "File trovato!", altrimenti stampa "File _non_ trovato"
+if [ -e $@ ]
+then
+	echo "il file e' presente"
+else
+	echo "il file non e' presente"
+fi
+
+exit 0
