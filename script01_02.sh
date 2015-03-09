6a7b54540125a9082715c819daaf097d92a0f865
ndtiralongo <Andreatiralongo12@gmail.com>
2015-03-09 14:56:04 +0100

    Update script01_02.sh

--- script01_02.sh
+++ script01_02.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 # Riceve un argomento da riga di comando, composto da un numero intero positivo
+echo "Inserire un argomento con numero intero positivo"
+exit 1
 # Scrive "Ciao 1", "Ciao 2", ... uno per riga, da 1 al numero passato come argomento
+for i in $(seq $1)
+do
+	echo "ciao $i"
+done
+
+exit 0
